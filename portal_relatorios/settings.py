9d7453a36c2d188ebacb67527b45ef7a5f0e7163
aleffazevedo <alleflinux@gmail.com>
2025-07-28 20:40:42 -0300

    Update settings.py

--- portal_relatorios/settings.py
+++ portal_relatorios/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-din-f*_fxrng%(mvk$(83
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ['.onrender.com', 'portal-relatorios.onrender.com']
 
 
 # Application definition
